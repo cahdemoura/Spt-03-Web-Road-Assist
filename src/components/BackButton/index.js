23a78540a49d6067845936a89f05cb9ac3da78f7
CahMoura <Caique.demoura0@gmail.com>
2023-09-17 13:12:20 -0300

    BackButton - Fix

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -1,14 +1,32 @@
 import ImagesComp from "../ImagesComp";
 
-export default function BackButton() {
+export default function BackButton({cor}) {
+    let color;
+    if(cor=="white"){
+        color= "#fff";}
+    else if(cor=="blue"){
+         color= "#BBD6FF";
+    }
     return (
         <div>
-            <button><ImagesComp src={"/images/backButton.png"} width={40} height={20}></ImagesComp></button>
+            <button><ImagesComp src={"/images/Arrow.svg"} width={32} height={24}></ImagesComp></button>
 
             <style jsx>{`
                 button{
                     border:none;
+                    padding: 6px 16px;
+                    display:flex;
+                    justify-content:center;
+                    align-items: center;
+                    border-radius: 18px;
+                    background:${color};
                 }
+
+                button:hover{
+                    cursor: pointer;
+                    opacity:.8;
+                }
+                
                 `}</style>
 
         </div>
